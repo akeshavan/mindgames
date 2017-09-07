56659778db1514aca0e79310c1bdb78f7717d8e7
akeshavan <keshavan@berkeley.edu>
2017-09-07 11:43:55 -0700

    fix: test upload too

--- python/upload2mindR.py
+++ python/upload2mindR.py
@@ -24,11 +24,16 @@ def load_json(filename):
     return data
 
 
-def upload_to_mindR(imgPath, task, subject, username, password):
+databases = {
+"prod": 'http://api.medulina.com/api/v1/',
+"dev": 'http://testapi.medulina.com/api/v1/'
+}
+
+def upload_to_mindR(imgPath, task, subject, username, password, database="prod"):
     api_token = "NnrP65CXaSnZ0aLPZ8Ox64d0pDlSKS0R8wpymwLr"
     # this code assumes that the jpg, the json describing it,
     # and the mask file have the same name with different extenstions
-    url = 'http://api.medulina.com/api/v1/'
+    url = databases[database]
     i = Path(imgPath)
     j = i.with_suffix('.json')
 
@@ -77,7 +82,7 @@ def upload_to_mindR(imgPath, task, subject, username, password):
                 headers={'Authorization':api_token},
                 #auth=(username, password)
                 )
-            
+
             print(rm.request.headers, rm.text)
             return rm
 
@@ -90,12 +95,13 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--subject", dest="subject")
     parser.add_argument("-u", "--username", dest="username")
     parser.add_argument("-p", "--password", dest="password")
+    parser.add_argument("-b", "--database", dest="database", default = "prod")
 
     args = parser.parse_args()
 
     all_images = glob(os.path.join(args.directory, "*.jpg"))
     print("found {} images".format(len(all_images)))
     for im in all_images:
-        rm = upload_to_mindR(im, args.task, args.subject, args.username, args.password)
+        rm = upload_to_mindR(im, args.task, args.subject, args.username, args.password, args.database)
         if rm == None:
             print("error with", im)
